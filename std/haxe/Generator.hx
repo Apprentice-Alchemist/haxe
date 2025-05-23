b250d307058f3a5f07d1968da38170e94fddb762
Apprentice-Alchemist <53486764+Apprentice-Alchemist@users.noreply.github.com>
2025-05-23 09:53:45 +0200

    tweak GeneratorIterator

--- std/haxe/Generator.hx
+++ std/haxe/Generator.hx
@@ -3,12 +3,12 @@ package haxe;
 import haxe.coro.CoroResult;
 
 private class GeneratorIterator<T> {
-	var fun:() -> CoroResult<T, Unit>;
+	var gen:Generator<T>;
 	var value:CoroResult<T, Unit>;
 
-	public inline function new(fun, value) {
-		this.fun = fun;
-		this.value = value;
+	public inline function new(gen: Generator<T>) {
+		this.gen = gen;
+		this.value = gen.resume();
 	}
 
 	public inline function hasNext() {
@@ -20,14 +20,15 @@ private class GeneratorIterator<T> {
 			case Yield(val): val;
 			case Ret(_): throw ".next called after end";
 		}
-		this.value = this.fun();
+		this.value = this.gen.resume();
 		return ret;
 	}
 }
 
 abstract Generator<T>(() -> CoroResult<T, Unit>) {
 	public inline function iterator():Iterator<T> {
-		return new GeneratorIterator(this, this());
+		// using `abstract` causes a typing error
+		return new GeneratorIterator(cast this);
 	}
 
 	public inline function resume():CoroResult<T, Unit> {
